6e146748907604ea1240961613ab5a14042d3826
Ankit Soni <ankit.soni@chef.io>
2024-01-08 17:08:41 +0530

    New Feature

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Ankit Soni'
 maintainer_email 'ankit.soni@progress.com'
 license 'All Rights Reserved'
 description 'Installs/Configures pipe'
-version '0.2.19'
+version '0.2.20'
 chef_version '>= 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
